26b8fcdec01f5dcae22085956cdb232efa985de3
jeferson <jeferson@jefersonfs>
2014-07-12 01:45:44 -0300

    pacote controle financeiro adicionado

---
+++ lp2-poupazudo/src/controleFinanceiro/testes/TestaUsuario.java
@@ -0,0 +1,36 @@
+package controleFinanceiro.testes;
+
+import org.junit.Test;
+
+public class TestaUsuario {
+
+	@Test
+	public void testUsuarioValido() {
+
+	}
+
+	@Test
+	public void testEmail() {
+
+	}
+
+	@Test
+	public void testNomeInvalido() {
+
+	}
+
+	@Test
+	public void testSenhaInvalido() {
+
+	}
+
+	@Test
+	public void testUsuariosIguais() throws Exception {
+
+	}
+
+	@Test
+	public void testToString() throws Exception {
+
+	}
+}
