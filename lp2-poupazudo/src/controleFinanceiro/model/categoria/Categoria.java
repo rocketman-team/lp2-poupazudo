15c40e6183078bcedaa586bf38a1aeb19e9a2fd0
jeferson <jeferson@jefersonfs>
2014-07-18 23:40:32 -0300

    adicionado uma nova categoria

--- lp2-poupazudo/src/controleFinanceiro/model/categoria/Categoria.java
+++ lp2-poupazudo/src/controleFinanceiro/model/categoria/Categoria.java
@@ -3,7 +3,7 @@ package controleFinanceiro.model.categoria;
 public enum Categoria {
 
 	ALIMENTACAO(1), EDUCACAO(2), LAZER(3), MORADIA(4), PAGAMENTOS(5), 
-	TRANSPORTE(6), ROUPA(7), SAUDE(8);
+	TRANSPORTE(6), ROUPA(7), SAUDE(8), ALUGUEL(9);
 
 	public int tipo;
 
