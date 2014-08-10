37534ac25adbf44bcc2d64fa6cf553fb15210b5c
Hebert Morais <hebertlucashllm@gmail.com>
2014-08-10 18:39:31 -0300

    Modificacao dos construtores Receita e Despesa

--- lp2-poupazudo/src/controleFinanceiro/model/transacao/Receita.java
+++ lp2-poupazudo/src/controleFinanceiro/model/transacao/Receita.java
@@ -8,14 +8,16 @@ import controleFinanceiro.model.conta.Conta;
 public class Receita extends Transacao {
 
 	public Receita(SimpleDateFormat data, double valor, Categoria categoria,
-			Recorrencia recorrencia, String descricao, Conta conta) {
-		super(data, valor, categoria, recorrencia, descricao, conta);
+			Recorrencia recorrencia, String descricao, Conta conta, int repeticao, boolean fixo) {
+		super(data, valor, categoria, recorrencia, descricao, conta, repeticao, fixo);
 	}
 
 	@Override
 	public void alteraSaldo(double valor) {
+		if (valor >= 0){
 		double somaSaldo = super.getConta().getSaldo() + valor;
 		super.getConta().setSaldo(somaSaldo);
+		}
 
 	}
 
