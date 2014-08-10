57e2bbed725fab720890d579a62d4394a83a6a1a
jeferson <jeferson@jefersonfs>
2014-08-10 00:18:07 -0300

    fixed

--- lp2-poupazudo/src/controleFinanceiro/model/transacao/Receita.java
+++ lp2-poupazudo/src/controleFinanceiro/model/transacao/Receita.java
@@ -6,16 +6,17 @@ import controleFinanceiro.model.Conta;
 import controleFinanceiro.model.categoria.Categoria;
 
 public class Receita extends Transacao {
-	
-	public Receita(SimpleDateFormat data, double valor, Categoria categoria, Recorrencia recorrencia, String descricao, Conta conta) {
-		super(data, valor, categoria, recorrencia, descricao, conta);	
+
+	public Receita(SimpleDateFormat data, double valor, Categoria categoria,
+			Recorrencia recorrencia, String descricao, Conta conta) {
+		super(data, valor, categoria, recorrencia, descricao, conta);
 	}
 
-@Override
+	@Override
 	public void alteraSaldo(double valor) {
 		double somaSaldo = super.getConta().getSaldo() + valor;
 		super.getConta().setSaldo(somaSaldo);
-		
+
 	}
-	
+
 }
